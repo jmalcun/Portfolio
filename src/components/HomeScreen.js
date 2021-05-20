fbfa39b08de4ce5da312cdc929f27eae6266da92
juan manuel malcun <jmalcun@gmail.com>
2021-05-19 23:31:03 -0300

    responsive-detailss

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -26,55 +26,37 @@ export const HomeScreen = () => {
             let scrollTop = document.documentElement.scrollTop;
             if(ancho < 450){
                 animado.forEach( elemento => {
-                    
                     elemento.style.opacity = 1;
                     elemento.className = "F-academica__card-grid animate__animated animate__fadeInRight animate__slower"
                     
                 })
                 animado2.forEach( elemento2 => {
-                    let alturaAnimado2 = elemento2.offsetTop;
-                    if(alturaAnimado2 - 550 < scrollTop){
-                        elemento2.className="apt__description animate__animated animate__fadeInRight animate__slower" 
-                    }
+                    elemento2.className="apt__description animate__animated animate__fadeInRight animate__slower" 
+                    
                 })
                 animado3.forEach( elemento3 => {
-                   
                     elemento3.style.opacity = 1;
                     elemento3.className ="apt-title animate__animated animate__fadeInLeft animate__slower "    
                     
                 })
                 animado4.forEach( elemento4 => {
-                    let alturaAnimado4 = elemento4.offsetTop;
-                    if(alturaAnimado4 - 550 < scrollTop){
-                        elemento4.style.opacity = 1;
-                        elemento4.className ="apt__icons animate__animated animate__fadeInLeft animate__slower"    
-                    } 
+                    elemento4.style.opacity = 1;
+                    elemento4.className ="apt__icons animate__animated animate__fadeInLeft animate__slower"    
+                     
                 })
                 animado5.forEach( elemento5 => {
-                    let alturaAnimado5 = elemento5.offsetTop;
-                    if(alturaAnimado5 - 600 < scrollTop){
-                        elemento5.style.opacity = 1;
-                        elemento5.className ="apt-title3 animate__animated animate__fadeInLeft animate__slow"    
-                    }
-                
+                    elemento5.style.opacity = 1;
+                    elemento5.className ="apt-title3 animate__animated animate__fadeInLeft animate__slow"    
                 })
                 animado6.forEach( elemento6 => {
-                    let alturaAnimado6 = elemento6.offsetTop;
-                    if(alturaAnimado6 - 550 < scrollTop){
-                        elemento6.style.opacity = 1;
-                        elemento6.className = "proyects-cards animate__animated animate__fadeInUp animate__slow"
-                    }  
-                })
-                animado7.forEach( elemento7 => {
-                    let alturaAnimado7 = elemento7.offsetTop;
-                    if(alturaAnimado7 - 550 < scrollTop){
-                        elemento7.style.opacity = 1;
-                        elemento7.className = "contact animate__animated animate__fadeInRight a   nimate__slower"
-                    }
+                    elemento6.style.opacity = 1;
+                    elemento6.className = "proyects-cards animate__animated animate__fadeInUp animate__slow"
                     
                 })
-
-
+                animado7.forEach( elemento7 => {
+                    elemento7.style.opacity = 1;
+                    elemento7.className = "contact animate__animated animate__fadeInRight a   nimate__slower" 
+                })
             }else{
                 animado.forEach( elemento => {
                     let alturaAnimado = elemento.offsetTop;
